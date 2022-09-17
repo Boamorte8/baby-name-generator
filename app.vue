ddf0a4983096a55f27bb6a9894e3525a96ace588
Esteban Salazar Zapata <esteban.salazar@Esteban-Salazar-MacBook-Pro.local>
2022-09-16 20:41:10 -0500

    start(project) first commit create project

---
+++ app.vue
@@ -0,0 +1,110 @@
+<script setup lang="ts">
+  import { Gender, Length, OptionsState, Popularity } from './models/optionsState';
+  import OptionButton from './components/OptionButton.vue';
+
+  const options = reactive<OptionsState>({
+    gender: Gender.GIRL,
+    popularity: Popularity.UNIQUE,
+    length: Length.SHORT,
+  });
+
+  const names = ref<string[]>([]);
+
+  const onClick = (prop: string, value: string) => {
+    options[prop] = value;
+  }
+</script>
+
+<template>
+  <div class="container">
+    <h1>Baby Name Generator</h1>
+
+    <p>Choose your options and click the "Find Names" buttom below</p>
+    <div class="options-container">
+      <div class="option-container">
+        <h4>1) Choose a gender</h4>
+        <div class="option-buttons">
+          <OptionButton :isActive="options.gender === Gender.BOY" @click="onClick('gender', Gender.BOY)">
+            Boy
+          </OptionButton>
+          <OptionButton :isActive="options.gender === Gender.UNISEX" @click="onClick('gender', Gender.UNISEX)">
+            Unisex
+          </OptionButton>
+          <OptionButton :isActive="options.gender === Gender.GIRL"  @click="onClick('gender', Gender.GIRL)">
+            Girl
+          </OptionButton>
+        </div>
+      </div>
+
+      <div class="option-container">
+        <h4>2) Choose the name's popularity</h4>
+        <div class="option-buttons">
+          <OptionButton :isActive="options.popularity === Popularity.TRENDY"
+                        @click="onClick('popularity', Popularity.TRENDY)">
+            Trendy
+          </OptionButton>
+          <OptionButton :isActive="options.popularity === Popularity.UNIQUE"
+                        @click="onClick('popularity', Popularity.UNIQUE)">
+            Unique
+          </OptionButton>
+        </div>
+      </div>
+
+      <div class="option-container">
+        <h4>3) Choose name's length</h4>
+        <div class="option-buttons">
+          <OptionButton :isActive="options.length === Length.LONG" @click="onClick('length', Length.LONG)">
+            Long
+          </OptionButton>
+          <OptionButton :isActive="options.length === Length.ALL" @click="onClick('length', Length.ALL)">
+            All
+          </OptionButton>
+          <OptionButton :isActive="options.length === Length.SHORT" @click="onClick('length', Length.SHORT)">
+            Short
+          </OptionButton>
+        </div>
+      </div>
+
+      <button class="primary">Find Names</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  font-family: Arial, Helvetica, sans-serif;
+  color: rgb(27, 60, 138);
+  max-width: 50rem;
+  margin: 0 auto;
+  text-align: center;
+}
+
+h1 {
+  font-size: 3rem;
+}
+
+.options-container {
+  background-color: rgb(255, 238, 236);
+  border-radius: 2rem;
+  padding: 1rem;
+  width: 95%;
+  margin: 0 auto;
+  margin-top: 4rem;
+  position: relative;
+}
+
+.option-container {
+  margin-bottom: 2rem;
+}
+
+.primary {
+  background-color: rgb(249, 87, 89);
+  color: white;
+  border-radius: 6.5rem;
+  border: none;
+  padding: .75rem 4rem;
+  font-size: 1rem;
+  margin-top: 1rem;
+  cursor: pointer;
+}
+</style>
---
+++ components/OptionButton.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+  const props = defineProps({
+    isActive: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  });
+</script>
+
+<template>
+  <button class="option"
+          :class="{ 'option-active': props.isActive }">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.option {
+  background: white;
+  outline: 0.15rem solid rgb(249, 87, 89);
+  border: none;
+  padding: 0.75rem;
+  width: 12rem;
+  font-size: 1rem;
+  color: rgb(27, 60, 138);
+  cursor: pointer;
+  font-weight: 200;
+}
+
+.option:first-child {
+  border-radius: 1rem 0 0 1rem;
+}
+
+.option:last-child{
+  border-radius: 0 1rem 1rem 0;
+}
+
+.option-active {
+  background-color: rgb(249, 87, 89);
+  color: white;
+}
+</style>
